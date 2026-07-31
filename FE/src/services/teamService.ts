ce90d9a70cedf79d79d421e7814422c60aec05b4
jronehuynh <jronehuynh@jronehuynhs-MacBook-Pro.local>
2026-07-31 15:19:50 +0700

    chore: resolve bbcrypt_jwt against Develop

--- FE/src/services/teamService.ts
+++
@@ -1,6 +0,0 @@
-import { api } from "../lib/apiClient";
-import type { TeamDto } from "../types/api";
-
-export async function fetchMyTeams(): Promise<TeamDto[]> {
-  return api<TeamDto[]>("/teams/my");
-}
